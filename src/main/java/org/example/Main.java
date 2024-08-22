ddda62427de058095b0b220b27ee3fd20f6e8121
Roman horodyskyi <gorod10516@gmail.com>
2024-08-22 22:16:28 +0300

    Check CI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         SumCalculator sumCalculator = new SumCalculator();
         System.out.println(sumCalculator.sum(3));
+        System.out.println(sumCalculator.sum(5));
     }
 }
